e0804caee1b2117395c0890aad9c31baed6cc684
Rafi <rafaloidion2@gmail.com>
2020-12-04 18:19:50 +0100

    close #7

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,7 +11,7 @@ import {RegData} from '../models/RegData';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+status = '';
   constructor(private router: Router, private messageService: MessageService, private registerService: RegisterService) {
   }
 
@@ -19,7 +19,17 @@ export class RegisterComponent implements OnInit {
   }
 
   registerUser(regForm: NgForm): void {
-    // tslint:disable-next-line:max-line-length
-    this.registerService.register(new RegData(regForm.value.city, regForm.value.email, regForm.value.firstName, regForm.value.houseNumber, regForm.value.lastName, regForm.value.nameAddress, regForm.value.password, regForm.value.postCode, regForm.value.street, regForm.value.username));
+    console.log(regForm.value);
+    this.registerService.register(new RegData(regForm.value.city, regForm.value.email, regForm.value.firstName, regForm.value.houseNumber,
+      regForm.value.lastName, regForm.value.nameAddress, regForm.value.password, regForm.value.postCode, regForm.value.street,
+      regForm.value.username)).subscribe(res => {
+      this.status = 'Rejestracja zakończona, na podany e-mail została wysłana wiadomość.';
+      this.messageService.add({ severity: 'success', summary: 'Sukces', detail: 'Rejestracja zakończona, na podany e-mail została wysłana wiadomość.'});
+      this.router.navigate(['/start']);
+    }, res => {
+        console.log(res);
+        this.status = res.error.message;
+        this.messageService.add({severity: 'error', summary: 'Blad', detail: res.error.message});
+    });
   }
 }
