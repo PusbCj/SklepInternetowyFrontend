6b7b11fbed6b64349de78613c9cf1c1716f428ba
Rafaloid <rafaloidion2@gmail.com>
2020-12-09 15:39:31 +0100

    Walidacja na stronie Moje dane + drobne poprawki

--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-my-account',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-account.component.css']
 })
 export class MyAccountComponent implements OnInit {
+changeData: FormGroup;
+changePassword: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.changeData = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      street: ['', Validators.required],
+      houseNumber: ['', Validators.required],
+      nameAddress: ['', Validators.required],
+      postCode: ['', Validators.required]
+    });
+
+    this.changePassword = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordRepeat: ['', [Validators.required]],
+    });
   }
 
+  // tslint:disable-next-line:typedef
+  get d() { return this.changeData.controls; }
+
+  // tslint:disable-next-line:typedef
+  get p() { return this.changePassword.controls; }
+
+  changeUserData(UserData: FormGroup): void{
+
+  }
+
+  changeUserPassword(UserData: FormGroup): void{
+
+  }
 }
