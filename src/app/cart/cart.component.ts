cef97e53153c7beb436d2ba383f71cc5ad29f022
Robert <cj92robert@gmail.com>
2020-12-13 21:31:21 +0100

    Add sum and minus function in shop cart.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {ProductServiceService} from '../services/product-service.service';
+import {Component, OnInit} from '@angular/core';
 import {Product} from '../models/Product.model';
 import {CartServiceService} from '../services/cart-service.service';
 import {ShopCart} from '../models/ShopCart.model';
@@ -13,6 +12,7 @@ import {ItemShopCart} from '../models/ItemShopCar.model';
 export class CartComponent implements OnInit {
   listproduct: Array<Product>;
   cart: ShopCart;
+  change = false;
 
 
   constructor(private cartService: CartServiceService) {
@@ -39,9 +39,12 @@ export class CartComponent implements OnInit {
   delete(item: ItemShopCart): void {
     // tslint:disable-next-line:triple-equals
     this.cart.itemShopCartList = this.cart.itemShopCartList.filter(x => x != item);
+    this.updateCart();
+  }
+
+  private updateCart(): void {
     this.cartService.updateCart(this.cart).subscribe(res => {
-        this.cart = res ;
-        if (res.itemShopCartList.length != null) {
+      if (res.itemShopCartList.length != null) {
         sessionStorage.setItem('items', res.itemShopCartList.length);
       } else {
         sessionStorage.setItem('items', String(0));
@@ -53,5 +56,26 @@ export class CartComponent implements OnInit {
 
   public sumCart(): number {
     return this.cart.itemShopCartList.map(item => item.quantity * item.product.price).reduce((a, b) => a + b, 0);
+  }
+
+  public minus(item: ItemShopCart): void {
+    item.quantity = item.quantity - 1;
+    if (item.quantity < 1) {
+      item.quantity = 1;
+    }
+    this.change = true;
+  }
+
+  public plus(item: ItemShopCart): void {
+    item.quantity += 1;
+    this.change = true;
+  }
+
+
+  updateIfNecessary(): void {
+    if (this.change === true) {
+      this.updateCart();
+      this.change = false;
+    }
   }
 }
